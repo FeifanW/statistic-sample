7cd9b3d208637f799e73e6103307307e64dfe623
FeifanW <qihang791@163.com>
2023-04-05 04:25:04 +0800

    统计信息存储功能开发

--- analysis.go
+++ analysis.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
+	"github.com/mediocregopher/radix.v2/pool"
+	"github.com/mgutz/str"
 	"github.com/sirupsen/logrus"
 	"io"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+const HANDLE_DIG = " /dig?"
+const HANDLE_MOVIE = "/movie/"
+const HANDLE_LIST = "/list/"
+const HANDLE_HTML = ".html"
 
 type cmdParams struct {
 	logFilePath string
@@ -23,15 +35,20 @@ type digData struct { // 对应js上报的数据格式
 
 // 做信息传输
 type urlData struct {
-	data digData
-	uid  string
+	data  digData
+	uid   string
+	unode urlNode
 }
 
 // 做信息存储
 type urlNode struct {
+	unType string // 详情页 或者 列表页 或者 首页
+	unRid  int    // Resource ID 资源ID
+	unUrl  string // 当前这个页面的url
+	unTime string // 当前访问这个页面的时间
 }
 
-type storageBlock struct {
+type storageBlock struct { // 用于存储的结构体
 	counterType  string  // 区分是什么统计
 	storageModel string  // 存什么样的格式，以什么形式存储
 	unode        urlNode // 存储内容
@@ -43,12 +60,11 @@ func init() {
 	// 这里配置第三方日志库logrus的配置
 	log.Out = os.Stdout             // 设置输出
 	log.SetLevel(logrus.DebugLevel) // 设置等级
-
 }
 
 func main() {
 	// 获取参数
-	logFilePath := flag.String("logFilePath", "/User/pangee/Public/nginx/log/dig.log", "log file path")
+	logFilePath := flag.String("logFilePath", "/Users/pangee/Public/nginx/log/dig.log", "log file path")
 	// 设置并发数
 	routineNum := flag.Int("routineNum", 5, "consumer number by go routine") // 如果不传的话默认是5
 	// 设置日志存放的地方
@@ -74,6 +90,20 @@ func main() {
 	var uvChannel = make(chan urlData, params.routineNum)
 	var storageChannel = make(chan storageBlock, params.routineNum)
 
+	// Redis pool 连接池
+	redisPool, err := pool.New("tcp", "localhost:6379", 2*params.routineNum) // 第三个参数是多大的并发度
+	if err != nil {
+		log.Fatalln("Redis pool created failed")
+		panic(err)
+	} else {
+		go func() {
+			for { // 防止连接池断开
+				redisPool.Cmd("PING")
+				time.Sleep(3 * time.Second)
+			}
+		}()
+	}
+
 	// 日志消费
 	go readFileLinebyLine(params, logChannel)
 	// 创建一组日志处理
@@ -82,30 +112,109 @@ func main() {
 	}
 	// 创建PV UV统计器
 	go pvCounter(pvChannel, storageChannel)
-	go uvCounter(uvChannel, storageChannel)
+	go uvCounter(uvChannel, storageChannel, redisPool)
 	// 如果还要加统计，可以在这里直接加
 
 	// 创建存储器
-	go dataStorage(storageChannel)
+	go dataStorage(storageChannel, redisPool)
 	time.Sleep(1000 * time.Second)
 }
 
-func dataStorage(storageChannel chan storageBlock) {
-
+// 把uv和pv的数据存储起来
+func dataStorage(storageChannel chan storageBlock, redisPool *pool.Pool) {
+	for block := range storageChannel { // 遍历每一个传递过来的消息
+		prefix := block.counterType + "_" // 区分是什么统计
+		// 10000 * N * M =
+		// 逐层添加，加洋葱皮的过程
+		// 维度： 天、小时、分钟
+		// 层级：定级-大分类-小分类-终极页面
+		// 存储模型：Redis SortedSet
+		setKeys := []string{ // 把每一个消息按照时分秒分类，添加到对应的redis集合中
+			prefix + "day_" + getTime(block.unode.unTime, "day"),
+			prefix + "hour_" + getTime(block.unode.unTime, "hour"),
+			prefix + "min_" + getTime(block.unode.unTime, "min"),
+			prefix + block.unode.unType + "_day_" + getTime(block.unode.unTime, "day"),
+			prefix + block.unode.unType + "_hour_" + getTime(block.unode.unTime, "hour"),
+			prefix + block.unode.unType + "_min_" + getTime(block.unode.unTime, "min"),
+		}
+		rowId := block.unode.unRid    // 路由里面的123.html数字编号
+		for _, key := range setKeys { // 遍历上面的切片
+			ret, err := redisPool.Cmd(block.storageModel, key, 1, rowId).Int() // int()表示从结果中提取整数值
+			if ret <= 0 || err != nil {
+				log.Errorln("DataStorage redis storage error.", block.storageModel, key, rowId)
+			} // 成功就不做输出
+		}
+	}
 }
 
 // 页面访问量
 func pvCounter(pvChannel chan urlData, storageChannel chan storageBlock) {
-
+	for data := range pvChannel {
+		SItem := storageBlock{"pv", "ZINCRBY", data.unode}
+		storageChannel <- SItem
+	}
 }
 
 // 用户访问量，需要根据用户id去重
-func uvCounter(pvChannel chan urlData, storageChannel chan storageBlock) {
-
+func uvCounter(uvChannel chan urlData, storageChannel chan storageBlock, redisPool *pool.Pool) {
+	for data := range uvChannel {
+		// HyperLoglog redis 行业一般用这个统计
+		// uv一般都是按天去重
+		hyperLogLogKey := "uv_hpll_" + getTime(data.data.time, "day")
+		// Redis PFADD命令将所有元素的参数保存在指定为第一个参数的键名的HyperLogLog数据结构
+		ret, err := redisPool.Cmd("PFADD", hyperLogLogKey, data.uid, "EX", 86400).Int()
+		if err != nil {
+			log.Warningln("UvCounter check redis hyperloglog failed")
+		}
+		if ret != 1 { // 执行命令的状态
+			continue
+		}
+		sItem := storageBlock{"uv", "ZINCRBY", data.unode}
+		storageChannel <- sItem
+	}
 }
 
-func logConsumer(logChannel chan string, pvChannel, uvChannel chan urlData) {
+func logConsumer(logChannel chan string, pvChannel, uvChannel chan urlData) error {
+	for logStr := range logChannel {
+		// 切割日志字符串，扣出打点上报的数据
+		data := cutLogFetchData(logStr) // 把需要的信息抽取出来
+		// uid
+		// 说明：课程模拟生成uid，md5(refer+ua)
+		hasher := md5.New()
+		hasher.Write([]byte(data.refer + data.ua))
+		uid := hex.EncodeToString(hasher.Sum(nil)) // 转成字符串
+		// 很多解析的工作可以放到这里完成
+		// ...
+		uData := urlData{data, uid, formatUrl(data.url, data.time)}
 
+		//log.Infoln(uData) //打印一条带有INFO级别的日志，并自动在结尾加上一个换行符
+
+		pvChannel <- uData
+		uvChannel <- uData
+	}
+	return nil
+}
+
+func cutLogFetchData(logStr string) digData { // 从每一条的日志中获取想要的结构
+	logStr = strings.TrimSpace(logStr) // 去除前后空格
+	pos1 := str.IndexOf(logStr, HANDLE_DIG, 0)
+	if pos1 == -1 { //没找到
+		return digData{}
+	}
+	pos1 += len(HANDLE_DIG)                             // 找到的话偏移量加上文本长度
+	pos2 := str.IndexOf(logStr, " HTTP/", pos1)         // 从pos1开始找
+	d := str.Substr(logStr, pos1, pos2-pos1)            // 把字符串截取出来
+	urlInfo, err := url.Parse("http://localhost/?" + d) // 因为url.Parse只能解析网址，所以要拼成完整的http://xxxx/?xxxx=xxx&xxx=xxx
+	if err != nil {
+		return digData{}
+	}
+	data := urlInfo.Query() // 解析query参数
+	return digData{         // 上报信息被解析
+		data.Get("time"),
+		data.Get("refer"),
+		data.Get("url"),
+		data.Get("ua"),
+	}
 }
 
 func readFileLinebyLine(params cmdParams, logChannel chan string) error {
@@ -121,6 +230,7 @@ func readFileLinebyLine(params cmdParams, logChannel chan string) error {
 		// 从缓冲区中读取字符串。它的作用是从缓冲区中读取指定长度的字符串，直到读取到指定的分隔符或者缓冲区的结尾
 		line, err := bufferRead.ReadString('\n')
 		logChannel <- line
+		log.Infof("line:" + line)
 		count++
 		if count%(1000*params.routineNum) == 0 {
 			log.Infof("ReadFilebyLine line:%d", count)
@@ -136,4 +246,44 @@ func readFileLinebyLine(params cmdParams, logChannel chan string) error {
 		}
 	}
 	return nil
+}
+
+func formatUrl(url, t string) urlNode {
+	// 一定从量大的着手，详情页>列表页>首页
+	pos1 := str.IndexOf(url, HANDLE_MOVIE, 0) // 字符串扣取
+	if pos1 != -1 {                           // url里面包含HANDLE_MOVIE
+		pos1 += len(HANDLE_MOVIE)
+		pos2 := str.IndexOf(url, HANDLE_HTML, 0)
+		idStr := str.Substr(url, pos1, pos2-pos1)
+		id, _ := strconv.Atoi(idStr) // 将字符串转成数字
+		return urlNode{"movie", id, url, t}
+	} else { // 列表页
+		pos1 = str.IndexOf(url, HANDLE_LIST, 0)
+		if pos1 != -1 {
+			pos1 += len(HANDLE_LIST)
+			pos2 := str.IndexOf(url, HANDLE_HTML, 0)
+			idStr := str.Substr(url, pos1, pos2-pos1)
+			id, _ := strconv.Atoi(idStr) // 将字符串转成数字
+			return urlNode{"list", id, url, t}
+		} else { // 首页
+			return urlNode{"home", 1, url, t}
+		} // 如果页面url有很多种，就不断在这里扩展
+	}
+}
+
+func getTime(logTime, timeType string) string {
+	var item string
+	switch timeType {
+	case "day":
+		item = "2006-01-02"
+		break
+	case "hour":
+		item = "2006-01-02 15"
+		break
+	case "min":
+		item = "2006-01-02 15:04"
+		break
+	}
+	t, _ := time.Parse(item, time.Now().Format(item)) //time.Parse是Go语言中用于将字符串转换为时间类型的函数
+	return strconv.FormatInt(t.Unix(), 10)            // 将整数转换成字符串形式
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module statistic-sample
 go 1.19
 
 require (
+	github.com/mediocregopher/radix.v2 v0.0.0-20181115013041-b67df6e626f9 // indirect
+	github.com/mediocregopher/radix/v3 v3.8.1 // indirect
+	github.com/mgutz/str v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 )
